b76ad1a5116d15411cf7cb4bc5c66fbed619b1ed
Jack O'Connor <jjoconnor15@gmail.com>
2020-08-02 21:08:44 -0500

    use some random colors in there too

--- keyboards/massdrop/alt/keymaps/jack/rgb_dazzlers/jack_effects.h
+++ keyboards/massdrop/alt/keymaps/jack/rgb_dazzlers/jack_effects.h
@@ -2,29 +2,40 @@ RGB_MATRIX_EFFECT(jack_test_effect)
 
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
+#include <stdlib.h>
 
 static uint8_t red_key_idx;
-#define JAK_EFFECT_EVERY 15
-static uint8_t every = JAK_EFFECT_EVERY;
+#define JAK_QMK_KB_EFFECT_EVERY 15
+static uint8_t every = JAK_QMK_KB_EFFECT_EVERY;
+
+#define JAK_QMK_KB_MAX_RAND 2147483647ul
 
 static void jack_test_effect_init(effect_params_t* params) {
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
     red_key_idx = led_min;
 }
 
+static uint8_t jak_random_byte(void) {
+    unsigned long l = (unsigned long) lrand48();
+    return l % 0x100;
+}
+
 static bool jack_test_effect_run(effect_params_t* params) {
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
     for (uint8_t i = led_min; i < led_max; i++) {
-        rgb_matrix_set_color(i, 0xff, 0xff, 0x00);
+        int am_not_red = ((int) red_key_idx != i);
+        rgb_matrix_set_color(i,
+                0xff,
+                0xff * am_not_red,
+                jak_random_byte() * am_not_red);
     }
-    rgb_matrix_set_color(red_key_idx, 0xff, 0x00, 0x00);
     every--;
     if (every == 0) {
         red_key_idx = (red_key_idx + 1) % led_max;
         if (red_key_idx == 0) {
             red_key_idx = led_min;
         }
-        every = JAK_EFFECT_EVERY;
+        every = JAK_QMK_KB_EFFECT_EVERY;
     }
     return led_max < DRIVER_LED_TOTAL;
 }
