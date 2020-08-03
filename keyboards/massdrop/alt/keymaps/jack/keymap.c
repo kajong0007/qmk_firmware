abf38784ea8413e69253954fe828d5365362e2b8
Jack O'Connor <jjoconnor15@gmail.com>
2020-08-02 20:32:04 -0500

    red letter sweeps keys, use unicodemap

--- keyboards/massdrop/alt/keymaps/jack/keymap.c
+++ keyboards/massdrop/alt/keymaps/jack/keymap.c
@@ -1,5 +1,19 @@
 #include QMK_KEYBOARD_H
 
+enum unicode_names {
+    SNEK,
+    RACCOON,
+    FOX,
+    WOLF,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [SNEK] = 0x1F40D,
+    [RACCOON] = 0x1F99D,
+    [FOX] = 0x1F98A,
+    [WOLF] = 0x1F43A,
+};
+
 enum alt_keycodes {
     U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
     U_T_AGCR,              //USB Toggle Automatic GCR control
@@ -18,14 +32,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, TT(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [1] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END, \
         _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
-        _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, _______,          KC_PGUP, KC_VOLD, \
-        _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END  \
+        _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, XP(RACCOON, FOX), \
+                                                                                                  XP(WOLF, SNEK), \
+                                                                                                           _______,          KC_PGUP, KC_VOLD, \
+        _______, _______, _______,                            _______,                            _______, TO(0), KC_HOME, KC_PGDN, KC_END  \
     ),
     /*
     [X] = LAYOUT(
--- keyboards/massdrop/alt/keymaps/jack/rgb_dazzlers/jack_effects.h
+++ keyboards/massdrop/alt/keymaps/jack/rgb_dazzlers/jack_effects.h
@@ -1,11 +1,42 @@
 RGB_MATRIX_EFFECT(jack_test_effect)
 
 #ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
-static bool jack_test_effect(effect_params_t* params) {
+
+
+static uint8_t red_key_idx;
+#define JAK_EFFECT_EVERY 15
+static uint8_t every = JAK_EFFECT_EVERY;
+
+static void jack_test_effect_init(effect_params_t* params) {
+    RGB_MATRIX_USE_LIMITS(led_min, led_max);
+    red_key_idx = led_min;
+}
+
+static bool jack_test_effect_run(effect_params_t* params) {
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
     for (uint8_t i = led_min; i < led_max; i++) {
-        rgb_matrix_set_color(i, 0xff, (i % 2) * 0xff, 0x00);
+        rgb_matrix_set_color(i, 0xff, 0xff, 0x00);
+    }
+    rgb_matrix_set_color(red_key_idx, 0xff, 0x00, 0x00);
+    every--;
+    if (every == 0) {
+        red_key_idx = (red_key_idx + 1) % led_max;
+        if (red_key_idx == 0) {
+            red_key_idx = led_min;
+        }
+        every = JAK_EFFECT_EVERY;
     }
     return led_max < DRIVER_LED_TOTAL;
 }
+
+static bool jack_test_effect(effect_params_t* params) {
+    if (params->init) jack_test_effect_init(params);
+    return jack_test_effect_run(params);
+}
+
+
+
+
+
+
 #endif
