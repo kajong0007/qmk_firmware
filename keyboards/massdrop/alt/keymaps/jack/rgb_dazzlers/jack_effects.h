6a2ed3eafc4e54cb3e51c3e869aea0d352ac8b9b
Jack O'Connor <jjoconnor15@gmail.com>
2020-08-04 07:58:48 -0500

    fix modifier keys, fix rgb toggle, steal other effects

--- keyboards/massdrop/alt/keymaps/jack/rgb_dazzlers/jack_effects.h
+++ keyboards/massdrop/alt/keymaps/jack/rgb_dazzlers/jack_effects.h
@@ -4,38 +4,94 @@ RGB_MATRIX_EFFECT(jack_test_effect)
 
 #include <stdlib.h>
 
-static uint8_t red_key_idx;
-#define JAK_QMK_KB_EFFECT_EVERY 15
-static uint8_t every = JAK_QMK_KB_EFFECT_EVERY;
+#define JAK_QMK_KB_EFFECT_EVERY 40
+//static uint8_t every = JAK_QMK_KB_EFFECT_EVERY;
 
 #define JAK_QMK_KB_MAX_RAND 2147483647ul
 
 static void jack_test_effect_init(effect_params_t* params) {
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
-    red_key_idx = led_min;
 }
 
-static uint8_t jak_random_byte(void) {
-    unsigned long l = (unsigned long) lrand48();
-    return l % 0x100;
-}
+//static uint8_t jak_random_byte(void) {
+//    unsigned long l = (unsigned long) lrand48();
+//    return l % 0x100;
+//}
+//
+//static RGB jak_random_color(void) {
+//    RGB ret = {jak_random_byte(), jak_random_byte(), jak_random_byte()};
+//    return ret;
+//}
+
+#define JAK_SET_PINK(i) rgb_matrix_set_color(i, 0xff, 100, 100)
+#define JAK_SET_BLUE(i) rgb_matrix_set_color(i, 0, 205, 252)
+#define JAK_SET_WHITE(i) rgb_matrix_set_color(i, 0xff, 0xff, 0xff)
+
+//static RGB nb_flag[] = {
+//    {0xff, 0xf4, 0x30},
+//    {0xff, 0xff, 0xff},
+//    {0x9c, 0x59, 0xd1},
+//    {0x00, 0x00, 0x00}
+//};
+//
+//static RGB lipstick_lesbian[] = {
+//    {0xd6, 0x29, 0x00},
+//    {0xff, 0x9b, 0x55},
+//    {0xff, 0xff, 0xff},
+//    {0xd4, 0x61, 0xa6},
+//    {0xa5, 0x00, 0x62}
+//};
+//
+//struct {
+//    uint8_t row_size;
+//} jak_row_handler;
+//struct {
+//    uint8_t row_size;
+//    struct {
+//        uint8_t index;
+//        RGB color;
+//    } swipe;
+//    uint8_t active_swipes;
+//    struct swipe current_swipes[3];
+//} typedef jak_row_holder;
 
 static bool jack_test_effect_run(effect_params_t* params) {
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
+    //HSV hsv = rgb_matrix_config.hsv;
+    //RGB col = hsv_to_rgb(hsv);
+    //uint16_t time = scale16by8(g_rgb_timer, rgb_matrix_config.speed / 8);
+
+    // call out to the rainbow pinwheel for the underglow if active
+    effect_params_t tmp = *params;
+    tmp.flags = tmp.flags & LED_FLAG_UNDERGLOW;
+    CYCLE_SPIRAL(&tmp);
     for (uint8_t i = led_min; i < led_max; i++) {
-        int am_not_red = ((int) red_key_idx != i);
-        rgb_matrix_set_color(i,
-                0xff,
-                0xff * am_not_red,
-                jak_random_byte() * am_not_red);
-    }
-    every--;
-    if (every == 0) {
-        red_key_idx = (red_key_idx + 1) % led_max;
-        if (red_key_idx == 0) {
-            red_key_idx = led_min;
+        RGB_MATRIX_TEST_LED_FLAGS();
+        if (!HAS_FLAGS(g_led_config.flags[i], LED_FLAG_UNDERGLOW)) {
+            uint8_t row = g_led_config.point[i].y;
+            switch(row) {
+                case 56:
+                    row = 0;
+                    rgb_matrix_set_color(i, 0xff, 0, 0);
+                    break;
+                case 44:
+                    row = 1;
+                    rgb_matrix_set_color(i, 0, 0xff, 0);
+                    break;
+                case 32:
+                    row = 2;
+                    rgb_matrix_set_color(i, 0, 0, 0xff);
+                    break;
+                case 19:
+                    row = 3;
+                    rgb_matrix_set_color(i, 0xff, 0, 0xff);
+                    break;
+                case 7:
+                    row = 4;
+                    rgb_matrix_set_color(i, 0, 0xff, 0xff);
+                    break;
+            }
         }
-        every = JAK_QMK_KB_EFFECT_EVERY;
     }
     return led_max < DRIVER_LED_TOTAL;
 }
